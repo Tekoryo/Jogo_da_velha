a8cdcc14a709b1a5a540d901cd0a684c1b8ef09b
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2021-08-30 15:17:29 -0300

    Elimando o buttom

--- js/Script.js
+++ js/Script.js
@@ -12,6 +12,7 @@ var res=document.getElementById("res")
 function jv(num){
   console.log(num)
   Nv+=1
+  console.log(`Nv é ${Nv}`)
   console.log(Nv)
   if(Nv%2==0){
     console.log("X")
@@ -72,47 +73,47 @@ function jv(num){
         console.log(num)
 
         b1.innerHTML="O"
-        b1=0
+        b1=2
         break;
       case 2:
         console.log()
         b2.innerHTML="O"
-        b2=0
+        b2=2
         break;
       case 3:
         console.log()
         b3.innerHTML="O"
-        b3=0
+        b3=2
         break;
       case 4:
         console.log()
         b4.innerHTML="O"
-        b4=0
+        b4=2
         break;
       case 5:
         console.log(num)
         b5.innerHTML="O"
-        b5=0
+        b5=2
         break;
       case 6:
         console.log()
         b6.innerHTML="O"
-        b7=0
+        b7=2
         break;
       case 7:
         console.log()
         b7.innerHTML="O"  
-        b7=0
+        b7=2
         breaḱ;
       case 8:
         console.log()
         b8.innerHTML="X"
-        b8=0
+        b8=2
         break;
       case 9:
         console.log()
         A9.innerHTML="X"
-        b9=0
+        b9=2
         res.innerHTML="ERRADO"
         break;
       default:
@@ -121,15 +122,12 @@ function jv(num){
         break;
     } 
   }
-  //"O" a soma total deve ser 6
+6
   if(b1+b2+b3 ==3 || b4+b5+b6 ==3 ||b7+b8+b9 ==3 || b1+b4+b7 ==3 || b2+b5+b8 ==3 ||b3+b6+b9 ==3||b1+b5+b9==3||b3+b5+b7==3){
     alert("X Ganhou!")
-  }else if(b1+b2+b3 ==0 || b4+b5+b6 ==0 ||b7+b8+b9 ==0 || b1+b4+b7 ==0 || b2+b5+b8 ==0 ||b3+b6+b9 ==0||b1+b5+b9==0||b3+b5+b7==0){
+  }else if(b1+b2+b3 ==6 || b4+b5+b6 ==6 ||b7+b8+b9 ==6 || b1+b4+b7 ==6 || b2+b5+b8 ==6 ||b3+b6+b9 ==6||b1+b5+b9==6||b3+b5+b7==6){
     alert("O Ganhou!")
-  }else{
+  }else if(Nv<=9){
     alert("Deu velha!")
-  }
-  if(Nv===9){
-    alert("Fim de Jogo!")
   }
 }

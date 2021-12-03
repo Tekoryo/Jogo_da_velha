b1b64d67d485b4ae6da22893a655033168decf1a
Thiago <Thiagocoelho.coelho2@gmail.com>
2021-12-02 21:14:54 -0300

    Deixando o codigo mais organizado

--- code/script.js
+++ code/script.js
@@ -1,73 +1,81 @@
-var Nv=0
+let Nv=0
 const X = [0,0,0,0,0,0,0,0,0]
 const O = [0,0,0,0,0,0,0,0,0]
 function jv(num){
-const img = document.querySelectorAll ("img") ; 
-var b1=document.getElementById("b1")
-var b2=document.getElementById("b2")
-var b3=document.getElementById("b3")
-var b4=document.getElementById("b4")
-var b5=document.getElementById("b5")
-var b6=document.getElementById("b6")
-var b7=document.getElementById("b7")
-var b8=document.getElementById("b8")
-var b9=document.getElementById("b9")
   Nv+=1
+  const b1=document.getElementById("b1")
+  const b2=document.getElementById("b2")
+  const b3=document.getElementById("b3")
+  const b4=document.getElementById("b4")
+  const b5=document.getElementById("b5")
+  const b6=document.getElementById("b6")
+  const b7=document.getElementById("b7")
+  const b8=document.getElementById("b8")
+  const b9=document.getElementById("b9")
   if(Nv%2==0){
     //Lado do player X
-    switch (num){
-      case 1:
-        b1.innerHTML="X"
-        X[0]=1
-        console.log(`B1=${X[1]}`)
-        break;
-      case 2:
-        b2.innerHTML="X"
-        X[1]=1
-        console.log(`B2=${X[2]}`)
-        break;
-      case 3:
-        b3.innerHTML="X"
-        X[2]=1
-        console.log(`B3=${X[3]}`)
-        break;
-      case 4:
-        b4.innerHTML="X"
-        X[3]=1
-        console.log(`B4=${X[4]}`)
-        break;
-      case 5:
-        b5.innerHTML="X"
-        X[4]=1
-        console.log(`B5=${X[5]}`)
-        break;
-      case 6:
-        b6.innerHTML="X"
-        X[5]=1
-        console.log(`B6=${X[6]}`)
-        break;
-      case 7:
-        b7.innerHTML="X"
-        X[6]=1
-        console.log(`B7=${X[7]}`)
-        break;
-      case 8:
-        b8.innerHTML="X"
-        X[7]=1
-        console.log(`B8=${X[8]}`)
-        break;
-      case 9:
-        b9.innerHTML="X"
-        X[8]=1
-        console.log(`B9=${X[9]}`)
-        break;
-      default:
-        alert("[ERRO!]")
-        break;
-    }
+    JogadorX(num)    
   }else{
     //Lado player O
-    switch(num){
+    JogadorO(num)   
+  }
+  // Logica de ganhador
+    Resultado(X,O)
+}
+const JogadorX = function(num){
+  switch (num){
+    case 1:
+      b1.innerHTML="X"
+      X[0]=1
+      console.log(`B1=${X[1]}`)
+      break;
+    case 2:
+      b2.innerHTML="X"
+      X[1]=1
+      console.log(`B2=${X[2]}`)
+      break;
+    case 3:
+      b3.innerHTML="X"
+      X[2]=1
+      console.log(`B3=${X[3]}`)
+      break;
+    case 4:
+      b4.innerHTML="X"
+      X[3]=1
+      console.log(`B4=${X[4]}`)
+      break;
+    case 5:
+      b5.innerHTML="X"
+      X[4]=1
+      console.log(`B5=${X[5]}`)
+      break;
+    case 6:
+      b6.innerHTML="X"
+      X[5]=1
+      console.log(`B6=${X[6]}`)
+      break;
+    case 7:
+      b7.innerHTML="X"
+      X[6]=1
+      console.log(`B7=${X[7]}`)
+      break;
+    case 8:
+      b8.innerHTML="X"
+      X[7]=1
+      console.log(`B8=${X[8]}`)
+      break;
+    case 9:
+      b9.innerHTML="X"
+      X[8]=1
+      console.log(`B9=${X[9]}`)
+      break;
+    default:
+      alert("[ERRO!]")
+      break;
+  }
+}
+const JogadorO = function(num){
+  switch(num){
     case 1:
         b1.innerHTML="O"
         O[1]=2
@@ -117,10 +125,8 @@ var b9=document.getElementById("b9")
         alert("[ERRO!]")
         break;
     } 
-  }
-  console.log(`X = ${X[0]+X[1]+X[2]+X[3]+X[4]+X[5]+X[6]+X[7]+X[8]+X[9]}`)
-  console.log(`O = ${O[1]+O[2]+O[3]+O[4]+O[5]+O[6]+O[7]+O[8]+O[9]}`)
-  // Logica de ganhador
+}
+const Resultado = function(X,O){
   if(X[0]+X[1]+X[2]==3 || X[3]+X[4]+X[5]==3 || X[6]+X[7]+X[8]==3 || X[0]+X[3]+X[6]==3 || X[1]+X[4]+X[7]==3 || X[2]+X[5]+X[8]==3 || X[0]+X[4]+X[8]==3 || X[2]+X[4]+X[6]==3){
     alert("[O Ganhador foi X!]")
   }else if(O[1]+O[2]+O[3]==6 || O[4]+O[5]+O[6]==6 || O[7]+O[8]+O[9]==6 || O[1]+O[4]+O[7]==6 || O[2]+O[5]+O[8]==6 || O[3]+O[6]+O[9]==6 || O[1]+O[5]+O[9]==6 || O[3]+O[5]+O[7]==6){

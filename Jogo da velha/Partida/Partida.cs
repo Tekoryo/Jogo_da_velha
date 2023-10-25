722d19217822cd9ee7c6a21a731491b3603d9a2a
thiago coelho <thiagocoelho.coelho2@gmail.com>
2023-10-25 15:06:00 -0300

    Zerando tudo

--- Jogo da velha/Partida/Partida.cs
+++ Jogo da velha/Partida/Partida.cs
@@ -2,45 +2,5 @@
 
 internal class Partida
 {
-    Tabuleiro tabuleiro = new Tabuleiro();
-    Jogador player = new Jogador();
-    VerificacaoVitoria verificacao = new VerificacaoVitoria();
-
-
-    public void IsPartida()
-    {
-        tabuleiro.SetIsVerificarVitoria();
-        tabuleiro.SetIsDadosInicio();
-        TipoJogador();
-        do
-        {           
-            Console.WriteLine($"Vez do {player.IdJogador()}:");            
-            tabuleiro.IsJogador(player);            
-            tabuleiro.IsVerificador(verificacao);            
-            tabuleiro.isGerenciador();
-            
-        } while (IsVerificacao());
-         
-        if(verificacao.SetIsVitoria()==1) verificacao.IsVitoiraDe(1);
-        else if(verificacao.SetIsVitoria()==-1) verificacao.IsVitoiraDe(-1);
-    }
-    private void TipoJogador()
-    {
-        Console.WriteLine("Qual Voce Escolhe Player1: [X/O]?");
-        string Player1 = Console.ReadLine()!;
-        player.TipoPlayer(Player1);
-    }
-    
-    private bool IsVerificacao()
-    {
-        if (verificacao.SetIsVitoria()==1) return false;
-        else if (verificacao.SetIsVitoria()==-1) return false;
-        return true;
-    }
-    public void IsVerificador(VerificacaoVitoria Verificador)
-    {
-        verificacao=Verificador;
-    }
-
 }
 
--- Jogo da velha/Player/Jogador.cs
+++ Jogo da velha/Player/Jogador.cs
@@ -1,59 +1,7 @@
-﻿using System.Numerics;
-
+﻿
 namespace Jogo_da_velha;
 
 internal class Jogador
 {
-    private string? Jogador1;
-    private string? Jogador2;
-    private int NumeroMovimento=1;
- 
-
-    public void SetContadorMovimento()
-    {
-        NumeroMovimento++;
-    }
-    public int GetContadorMovimento()
-    {
-       return NumeroMovimento;
-    }
-    public string GetIsJogador(int ID)
-    {
-        if(ID == 1)return Jogador1;
-        else return Jogador2;
-       
-    }
-    public void TipoPlayer(string Player1)
-    {
-        string Player2;
-       
-        if (Player1 == "x") Player2 = "o";
-        else Player2 = "x";
-
-        Jogador1 = Player1.ToUpper();
-        Jogador2 = Player2.ToUpper();
-    }
-
-    public string IsJogada()
-    {
-        Console.WriteLine("\n Sua Jogada:");
-        string posicao = Console.ReadLine()!;
-        return posicao;        
-    }
-
-    public string IsJogador() 
-    {        
-        if (NumeroMovimento % 2 == 0) return GetIsJogador(2);
-        else return GetIsJogador(1);
-    } 
-    public string IdJogador()
-    {
-       if (IsJogador()==GetIsJogador(2)) return $"Jogador 2 {GetIsJogador(2)}";
-       else return $"Jogador 1 {GetIsJogador(1)}";
-    }
-    public string IdJogadorVitoria()
-    {
-       if (IsJogador()==GetIsJogador(2)) return $"Jogador 1";
-       else return $"Jogador 2";
-    }
+   
 }

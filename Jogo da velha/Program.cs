fdff0e293b82911f75ce2ed3b40a557b9320ccec
thiago coelho <thiagocoelho.coelho2@gmail.com>
2023-10-25 12:13:46 -0300

    Melhorando menu

--- Jogo da velha/Program.cs
+++ Jogo da velha/Program.cs
@@ -1,14 +1,25 @@
 ﻿using Jogo_da_velha.Partida;
 
-Console.WriteLine("Jogo da Velha!");
 
 Partida partida = new Partida();
 
 void menu()
 {
+    string NPartida = "s";
+    do {
+        NovaPartida();
+
+        Console.WriteLine("Quer mais uma partida? [s/n]");
+        NPartida = Console.ReadLine();
+    } while(NPartida!="n");    
+        
+}
+void NovaPartida()
+{
     partida.IsPartida();
 
 }
+
+Console.WriteLine("Jogo da Velha!");
 menu();
 Console.WriteLine("Fim!");
- 
